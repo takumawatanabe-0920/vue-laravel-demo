151a14e3d67750d3648c5d33860a4f6a1351132d
takuma <takumaozk0920@gmail.com>
2024-02-11 19:00:57 -0500

    first commit

---
+++ backend/routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/', function () {
+    return 'Hello, World!';
+});
+
+Route::get('/2', function () {
+    return
+    [
+        'name' => 'Laravel',
+        'version' => '8.x'
+    ];
+});
+
